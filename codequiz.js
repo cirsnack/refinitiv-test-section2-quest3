0c3d53e419416221e7723e67ef1ca7f6415cc4fb
cirsnack <cirsnackz@gmail.com>
2021-06-21 11:58:37 +0700

    update code

--- codequiz.js
+++ codequiz.js
@@ -1,54 +1,75 @@
 const fetch = require('node-fetch');
-const page_in_text_string = 'https://codequiz.azurewebsites.net';
-const jsdom = require('jsdom');
+const urlFund = "https://codequiz.azurewebsites.net/";
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+// Create function for Retrieve data from page
 async function retrieveNav() {
     let result = null;
+
+    // There's require opts to retrieve data as cookie: 'hasCookie=true'
     const opts = {
         headers: {
             cookie: 'hasCookie=true'
         }
     };
-    await fetch(page_in_text_string, opts)
+
+    // Use fetch modul for retrieve with opts
+    await fetch(urlFund,opts)
     .then(res => res.text())
-    .then((response) =>    {
-        result = response;
-    });
+    .then(res => {
+        result = res;
+    })
     return result;
 }
 
-async function exec () {
-    const nav =  await retrieveNav();
-    const dom = new jsdom.JSDOM(nav.trim());
-    const tr = dom.window.document.getElementsByTagName("tr");
+// Create exec function for init this file
+async function exec() {
+
+    // Use nav for get result from retrieveNav function
+    // Use JSDOM to transform nav to HTML cause nav is string that can't tag by anything
+    const nav = await retrieveNav();
+    const dom = new JSDOM(nav.trim());
+    const tr = dom.window.document.getElementsByTagName("tr")
+    
+    //Create fundList to keep fundName and fundNav for express 
     const fundList = [];
-    for(let index = 0; index< tr.length; index ++) {
-        const obj = { fundName: null, fundNav: null};
-        const trItem = tr.item(index);
-        let fundNameIndex = 0;
-        let fundNavIndex = 1;
-        for(let indexTd = 0; indexTd < trItem.childNodes.length; indexTd ++) {
-            const tdItem = trItem.childNodes.item(indexTd);
+    for(let index = 0; index < tr.length; index++) {
+        const obj = {fundName: null , fundNav: null};
+        const trItem= tr.item(index);
+        const trItemLength = trItem.childNodes.length
+        let indexForName = 0;
+        let indexForNav = 1;
+        for(let tdIndex = 0; tdIndex < trItemLength; tdIndex++){
+            const tdItem = trItem.childNodes.item(tdIndex);
+            console.log('original item from td', tdItem.textContent)
+            // There are empties content in HTML so have to prevent it before
             if(tdItem.textContent === ' ') {
-                fundNameIndex = + 1;
-                fundNavIndex = + 2;
+                indexForName += 1;
+                indexForNav += 1;
+            // Then keep fundName and fundNave in same object
             } else {
-                if(indexTd === fundNameIndex) {
+                if(tdIndex === indexForName) {
                     obj.fundName = tdItem.textContent.trim();
-                } else if (indexTd === fundNavIndex){
+                } else if(tdIndex === indexForNav) {
                     obj.fundNav = tdItem.textContent;
                 }
             }
-            
+           
         }
-        fundList.push(obj)
+        fundList.push(obj);
     }
-    var myArgs = process.argv.slice(2).toString();
-    const foundFund = fundList.find(el=> el.fundName === myArgs);
-    if(foundFund) {
-        console.log(foundFund.fundNav)
+    console.log('fundList', fundList)
+
+    // Create variable to keep argument when running
+    // find obj which fundName match with myArgs and keep the obj
+    const myArgs = process.argv.slice(2).toString();
+    const foundFun = fundList.find(item => item.fundName === myArgs)
+    // If it match console.log fundNav
+    if(foundFun) {
+        console.log(foundFun.fundNav)
     } else {
-        console.log('Not Found FUND')
+        console.log('Not found')
     }
 }
-
 exec();
